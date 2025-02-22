bb85c2849f39ae6bee32815d0d843980e5a2f08f
Ossama <ossamatammam@gmail.com>
2025-02-22 18:01:15 +0200

    Add unique constraint for the user migration

--- services/user-service/app/models/user.rb
+++ services/user-service/app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   self.table_name = "user_service.users"
-  validates :user_id, presence: true
-  validates :username, presence: true
+  validates :user_id, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true
   validates :name, presence: true
-  validates :email, presence: true
+  validates :email, presence: true, uniqueness: true
 end
--- services/user-service/db/migrate/20250222141044_create_users_table.rb
+++ services/user-service/db/migrate/20250222141044_create_users_table.rb
@@ -2,13 +2,17 @@ class CreateUsersTable < ActiveRecord::Migration[8.0]
   def change
     create_table :users, id: false do |t|
       t.uuid :user_id, primary_key: true
-      t.string :username, null: false, unique: true
+      t.string :username, null: false
       t.string :name, null: false
-      t.string :email, null: false, unique: true
+      t.string :email, null: false
       t.boolean :email_verified
       t.string :avatar_url
       t.timestamp :deleted_at
       t.timestamps
     end
+
+    # Unique constraints
+    add_index :users, :username, unique: true
+    add_index :users, :email, unique: true
   end
 end
