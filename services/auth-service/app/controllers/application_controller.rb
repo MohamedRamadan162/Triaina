58bca46ecbcc5f7c2b828ee6cd0b7b89b797dceb
Ossama <ossamatammam@gmail.com>
2025-03-04 02:18:21 +0200

    init auth service

---
+++ services/auth-service/app/controllers/application_controller.rb
@@ -0,0 +1,7 @@
+class ApplicationController < ActionController::API
+  include ResponseHandler
+  include ErrorHandler
+  include Pundit::Authorization
+  include Pagy::Backend
+  include ActionController::Cookies
+end
