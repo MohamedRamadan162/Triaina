1ea4b2e545e353d36532db89c097fa9f67244b91
Ossama <ossamatammam@gmail.com>
2025-05-26 01:02:31 +0300

    init swagger

--- services/triaina/config/routes.rb
+++ services/triaina/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "health" => "health_check#show", as: :health_check
   get "ready" => "health_check#dependencies", as: :ready_check
