79b57e4f7aebdef2f87ba473cf6e013b7c4f56b3
Ossama <ossamatammam@gmail.com>
2025-06-05 19:10:39 +0300

    ?

--- services/triaina/db/schema.rb
+++ services/triaina/db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema[8.0].define(version: 2025_05_29_152227) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+  enable_extension "pgcrypto"
 
   create_table "abilities", force: :cascade do |t|
     t.string "name"
