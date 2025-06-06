60cbbfd9836fe03ac40d47dc3a4e75fa4a7dfce5
Ossama <ossamatammam@gmail.com>
2025-06-06 23:00:50 +0300

    CICD Try

--- services/triaina/spec/tests/requests/api/v1/user/course_spec.rb
+++ services/triaina/spec/tests/requests/api/v1/user/course_spec.rb
@@ -1,36 +1,36 @@
-# # frozen_string_literal: true
+# frozen_string_literal: true
 
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe 'Enrolled Courses', type: :request do
-#   before do
-#     @user = create(
-#       :user,
-#       email: TestConstants::DEFAULT_USER[:email],
-#       username: TestConstants::DEFAULT_USER[:username],
-#       name: TestConstants::DEFAULT_USER[:name]
-#     )
-#     @course = create(:course)
-#     @course_2 = create(:course)
-#     @enrollment = create(:enrollment, user: @user, course: @course)
-#   end
+RSpec.describe 'Enrolled Courses', type: :request do
+  before do
+    @user = create(
+      :user,
+      email: TestConstants::DEFAULT_USER[:email],
+      username: TestConstants::DEFAULT_USER[:username],
+      name: TestConstants::DEFAULT_USER[:name]
+    )
+    @course = create(:course)
+    @course_2 = create(:course)
+    @enrollment = create(:enrollment, user: @user, course: @course)
+  end
 
-#   context "when user is enrolled in a course" do
-#     before do
-#       post "#{TestConstants::DEFAULT_API_BASE_URL}/auth/login", params: { email: @user.email, password: TestConstants::DEFAULT_USER[:password] }
-#       get_enrolled_courses
-#     end
-#     it 'returns user enrolled courses' do
-#       response_body = JSON.parse(response.body)
-#       expect(response_body['success']).to be_truthy
-#       expect(response_body['courses'].length).to eq(1)
-#       expect(response_body['courses'][0]['id']).to eq(@course.id)
-#     end
-#   end
+  context "when user is enrolled in a course" do
+    before do
+      post "#{TestConstants::DEFAULT_API_BASE_URL}/auth/login", params: { email: @user.email, password: TestConstants::DEFAULT_USER[:password] }
+      get_enrolled_courses
+    end
+    it 'returns user enrolled courses' do
+      response_body = JSON.parse(response.body)
+      expect(response_body['success']).to be_truthy
+      expect(response_body['courses'].length).to eq(1)
+      expect(response_body['courses'][0]['id']).to eq(@course.id)
+    end
+  end
 
-#   private
+  private
 
-#   def get_enrolled_courses
-#     get "#{TestConstants::DEFAULT_API_BASE_URL}/users/#{@user.id}/courses"
-#   end
-# end
+  def get_enrolled_courses
+    get "#{TestConstants::DEFAULT_API_BASE_URL}/users/#{@user.id}/courses"
+  end
+end
