c8bb14439fac07d29d72247e449b89cf867f3683
Ossama <ossamatammam@gmail.com>
2025-06-06 21:23:06 +0300

    caching chat channels

--- services/triaina/app/channels/course_chat_channel.rb
+++ services/triaina/app/channels/course_chat_channel.rb
@@ -1,9 +1,12 @@
 class CourseChatChannel < ApplicationCable::Channel
   include Pagy::Backend
+  identified_by :current_user, :channel_id
 
   def subscribed
-    chat_channel = ChatChannel.find(params[:channel_id])
-    stream_for chat_channel
+    self.current_user = connection.current_user
+    self.channel_id = params[:channel_id]
+    @chat_channel = ChatChannel.find(channel_id)
+    stream_for @chat_channel
   end
 
   def unsubscribed
@@ -11,22 +14,20 @@ class CourseChatChannel < ApplicationCable::Channel
 
   # Called by client to send message
   def send_message(data)
-    chat_channel = ChatChannel.find(params[:channel_id])
-    message = chat_channel.chat_messages.create!(
-      user: connection.current_user,
+    message = @chat_channel.chat_messages.create!(
+      user: current_user,
       content: data["message"]
     )
-    CourseChatChannel.broadcast_to(chat_channel, {
+    CourseChatChannel.broadcast_to(@chat_channel, {
       message: render_message(message)
     })
   end
 
   def update_message(data)
-    chat_channel = ChatChannel.find(params[:channel_id])
-    message = chat_channel.chat_messages.find(data["message_id"])
-    if message.user == connection.current_user
+    message = @chat_channel.chat_messages.find(data["message_id"])
+    if message.user == current_user
       message.update!(content: data["content"])
-      CourseChatChannel.broadcast_to(chat_channel, {
+      CourseChatChannel.broadcast_to(@chat_channel, {
         message: render_message(message)
       })
     else
@@ -35,11 +36,10 @@ class CourseChatChannel < ApplicationCable::Channel
   end
 
   def delete_message(data)
-    chat_channel = ChatChannel.find(params[:channel_id])
-    message = chat_channel.chat_messages.find(data["message_id"])
-    if message.user == connection.current_user
+    message = @chat_channel.chat_messages.find(data["message_id"])
+    if message.user == current_user
       message.destroy!
-      CourseChatChannel.broadcast_to(chat_channel, {
+      CourseChatChannel.broadcast_to(@chat_channel, {
         message_id: message.id
       })
     else
@@ -48,10 +48,9 @@ class CourseChatChannel < ApplicationCable::Channel
   end
 
   def fetch_messages(data)
-    chat_channel = ChatChannel.find(params[:channel_id])
-    pagy, messages = pagy(chat_channel.chat_messages.order(created_at: :asc), items: 100, page: data["page"] || 1)
+    pagy, messages = pagy(@chat_channel.chat_messages.order(created_at: :asc), items: 100, page: data["page"] || 1)
 
-    CourseChatChannel.broadcast_to(chat_channel, {
+    CourseChatChannel.broadcast_to(@chat_channel, {
       messages: messages.map { |msg| render_message(msg) },
       pagy: {
         page: pagy.page,
@@ -63,7 +62,7 @@ class CourseChatChannel < ApplicationCable::Channel
     })
 
   rescue Pagy::OverflowError
-    CourseChatChannel.broadcast_to(chat_channel, {
+    CourseChatChannel.broadcast_to(@chat_channel, {
       type: "error",
       message: "Page not found"
     })
