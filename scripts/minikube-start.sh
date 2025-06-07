b27988547cfa4ef9e8fd99f240c582b33954210c
Ossama <ossamatammam@gmail.com>
2025-06-07 21:19:47 +0300

    K8S working

--- scripts/minikube-start.sh
+++ scripts/minikube-start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Start minikube with Docker driver and enable registry addon
-minikube start --driver=docker --addons=registry --insecure-registry="10.0.0.0/24"
+minikube start --driver=docker
 echo "Minikube started"
 
-# Set up registry proxy
-docker run -d --name registry-proxy --rm -it --network=host alpine ash -c "apk add socat && socat TCP-LISTEN:5000,reuseaddr,fork TCP:$(minikube ip):5000"
-echo "Registry proxy set up at localhost:5000"
+# # Set up registry proxy
+# docker run -d --name registry-proxy --rm -it --network=host alpine ash -c "apk add socat && socat TCP-LISTEN:5000,reuseaddr,fork TCP:$(minikube ip):5000"
+# echo "Registry proxy set up at $(minikube ip):5000"
 
 kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
 kubectl apply -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.2.0/standard-install.yaml
--- scripts/minikube-stop.sh
+++ scripts/minikube-stop.sh
@@ -2,5 +2,4 @@
 
 minikube stop
 minikube delete
-docker rm -f registry-proxy
 echo "Minikube stopped and deleted, registry proxy removed."
